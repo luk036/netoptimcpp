e42485431b10d5b280c6474e248c9b4d7c8417bc
Wai-Shing Luk <luk036@gmail.com>
2019-11-14 21:33:41 +0800

    init r0 by caller

--- lib/include/netoptim/min_cycle_ratio.hpp
+++ lib/include/netoptim/min_cycle_ratio.hpp
@@ -23,40 +23,12 @@
  * @param dist
  * @return auto
  */
-template <typename Graph, typename Fn1, typename Fn2, typename Container>
-auto min_cycle_ratio(Graph& G, Fn1 get_cost, Fn2 get_time, Container& dist, size_t max_iter=1000)
+template <typename Graph, typename T, typename Fn1, typename Fn2, typename Container>
+auto min_cycle_ratio(Graph& G, T r0, Fn1 get_cost, Fn2 get_time, Container& dist, size_t max_iter=1000)
 {
     using edge_t = typename Graph::edge_t;
-    using T = typename Container::value_type;
-
-    edge_t e0;
-    for (auto e : G.edges())
-    {
-        e0 = e; // get the first edge (better idea???)
-        break;
-    }
-
-    // auto& edges = G.edges();
-    // auto max_c = *std::max_element(edges.begin(), edges.end(),
-    //                 [](const edge_t& e) { return get_cost(G, e); });
-
-    // auto max_cost = *std::max_element(cost.begin(), cost.end());
-    // auto min_time = *std::min_element(time.begin(), time.end());
-    auto max_cost = get_cost(e0);
-    auto min_time = get_time(e0);
-    for (auto e : G.edges())
-    {
-        const auto c = get_cost(e);
-        const auto t = get_time(e);
-        if (max_cost < c)
-            max_cost = c;
-        if (min_time > t)
-            min_time = t;
-    }
-    auto r0 = T(max_cost * G.number_of_edges()) / min_time;
-
-    using cost_T = decltype(get_cost(e0));
-    using time_T = decltype(get_time(e0));
+    using cost_T = decltype(get_cost(std::declval<edge_t>()));
+    using time_T = decltype(get_time(std::declval<edge_t>()));
 
     auto calc_ratio = [&](auto& C) {
         auto total_cost = cost_T(0);
